fc9c7256f51e9a4d6a89e6629cddf6f6836a5dc1
robsoneducardo <robsoneducardo@gmail.com>
2021-12-15 15:53:57 -0300

    adição e edição de produto

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -12,4 +12,31 @@ class ProdutoController extends Controller
         $produtos = Produto::all();
         return view('produto.index', ['produtos'=>$produtos]);
     }
+
+    public function create(){
+        return view('produto.create');
+    }
+
+    public function store(Request $request){
+        Produto::create($request->all());
+        return redirect()->route('produto.index');
+    }
+
+    public function edit($id){
+        // dd($id);
+        $produto = Produto::where('id', $id)->first();
+        if (!empty($produto))
+            return view('produto.edit', ['produto'=>$produto]);
+        else
+            return redirect()->route('produto.index');
+    }
+
+    public function update(Request $request){
+        $data = [
+            'nome' => $request->nome,
+        ];
+        Produto::where('id', $request->id)->update($data);
+        return redirect()->route('produto.index');
+    }
+
 }
